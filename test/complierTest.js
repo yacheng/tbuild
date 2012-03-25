6d65c03339ef575828751cb3b438525886321e7d
czy88840616 <czy88840616@gmail.com>
2012-03-25 15:44:45 +0800

    add npm

---
+++ test/complierTest.js
@@ -0,0 +1,10 @@
+var ModuleComplier = require('../lib/base').ModuleComplier;
+
+ModuleComplier.build({
+    target:'/tc/cart',
+    base:'D:\\project\\tradeface\\assets\\4.0',
+    debug: false,
+    inputEncoding:'gbk',
+    outputEncoding:'gbk',
+    output:'D:\\project\\tradeface\\assets\\120324'
+});
